3ff41b9820eae294b3411be1e0959919d1613d85
kevin <kevinhuaman2018@gmail.com>
2025-07-11 23:48:57 -0500

    cambio

--- app/api/health.py
+++ app/api/health.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel, Field
+
 from app.core.security import get_current_user
 from app.models import ml
-from pydantic import BaseModel, Field
 
 router = APIRouter()
 
 class FeaturesDict(BaseModel):
-    data: dict[str, float] = Field(..., description="clave=nombre columna, valor=numérico")
+    data: dict[str, float] = Field(..., description="clave=nombre columna, valor numérico")
 
 @router.post("/predictions")
 def make_prediction(body: FeaturesDict, current_user: str = Depends(get_current_user)):
     pred = ml.predict_from_dict(body.data)
     return {"user": current_user, "prediction": pred}
-       
